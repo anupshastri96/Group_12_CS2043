b806c85926213999d2cff25f9bee10eaa69a4131
Denver Dias <denverkdias@gmail.com>
2023-11-25 17:30:42 -0400

    Mini Update

--- src/java/SecondaryDriver.java
+++ src/java/SecondaryDriver.java
@@ -63,35 +63,73 @@ public class Secondary_Driver {
             this.connection = connection;
         }
 
-    @Override
-    public void run() {
-        // Implemented the logic to update the current stop index for the bus route
-        BusRoute busRoute = new BusRoute(busRouteId, connection);
-        int currentStopIndex = busRoute.getCurrentStopIndex();
-        int lastStopIndex = busRoute.getRoute().getStops().size() - 1;
+        @Override
+        public void run() {
+            BusRoute busRoute = new BusRoute(busRouteId, connection);
 
-        //update the current stop ID
-        int newStopIndex = (currentStopIndex < lastStopIndex) ? currentStopIndex + 1 : 0;
+            // Implement the logic for cyclic traversal of stops
+            int currentStopIndex = busRoute.getCurrentStopIndex();
 
-        try {
-            String updateQuery = "UPDATE bus_route SET current_stop_index = ? WHERE bus_route_id = ?"; //updating the current stop ID in the database
+            // Retrieve the stops for the route from the database
+            String stopsQuery = "SELECT * FROM stops WHERE route_id = ? ORDER BY stop_index";
+            try (PreparedStatement stopsStatement = connection.prepareStatement(stopsQuery)) {
+                stopsStatement.setInt(1, busRoute.getRoute().getId());
+                ResultSet stopsResultSet = stopsStatement.executeQuery();
+
+                int lastStopIndex = -1;
+                while (stopsResultSet.next()) {
+                    lastStopIndex = stopsResultSet.getInt("stop_index");
+                }
+
+                if (currentStopIndex < lastStopIndex) {
+                    // Increment the stop index
+                    int newStopIndex = currentStopIndex + 1;
+
+                    // Update the current stop index in the database
+                    updateCurrentStopIndex(newStopIndex);
+
+                    System.out.println("Current stop index updated successfully for Bus Route " + busRouteId);
+                } else {
+                    // Reset to the first stop (cyclic traversal)
+                    int newStopIndex = 0;
+
+                    // Update the current stop index in the database
+                    updateCurrentStopIndex(newStopIndex);
+
+                    System.out.println("Reached the end of stops. Resetting to the first stop for Bus Route " + busRouteId);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void updateCurrentStopIndex(int newStopIndex) throws SQLException {
+            String updateQuery = "UPDATE bus_route SET current_stop_index = ? WHERE bus_route_id = ?";
             try (PreparedStatement preparedStatement = connection.prepareStatement(updateQuery)) {
                 preparedStatement.setInt(1, newStopIndex);
                 preparedStatement.setInt(2, busRouteId);
-
-                int rowsAffected = preparedStatement.executeUpdate();
-
-                if (rowsAffected > 0) {
-                    System.out.println("Current stop index updated successfully for Bus Route " + busRouteId);
-                } else {
-                    System.out.println("Failed to update current stop index for Bus Route " + busRouteId);
-                }
+                preparedStatement.executeUpdate();
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
-    }
-
     }
 }
 
+/*
+ Database Connection: It establishes a connection to a MySQL database hosted at "sql9.freesqldatabase.com" on port 3306, using the username "sql9657484" and password "e8X5f44Fl9". The initializeDatabaseConnection method returns a Connection object.
+Initialization of Bus Routes:
+
+The initializeBusRoutes method retrieves data from the "bus_route" table in the database.
+For each row in the result set, it creates a BusRoute object using the constructor that takes a busRouteId and a Connection as parameters.
+The BusRoute objects are added to a list (busRoutes), which is returned.
+
+UpdateTask Class:This is a nested class within Secondary_Driver that extends TimerTask.
+It has a constructor that takes a busRouteId and a Connection.
+The run method is where the logic for updating the current stop index for a bus route is implemented.
+It creates a BusRoute object using the provided busRouteId and Connection.
+It calculates the new stop index based on the current stop index and the total number of stops in the route.
+It then performs an SQL update on the "bus_route" table to set the current stop index to the calculated value.
+It prints success or failure messages based on the result of the update.
+
+Main Method (in Secondary_Driver class): It initializes the database connection, creates a Timer object, and retrieves a list of bus routes using the initializeBusRoutes method.
+For each bus route, it creates an UpdateTask object and schedules it to run every 30 seconds.
+ */
